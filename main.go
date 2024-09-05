2d7fcb9315ff291a188b5960e1794e8187ecc9bd
JMjirapat <jirapat.prt@gmail.com>
2024-09-06 00:44:06 +0700

    add: more path to hello

--- main.go
+++ main.go
@@ -7,5 +7,9 @@ func main() {
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Status(200).SendString("Hello world!")
 	})
+	app.Get("/:name", func(c *fiber.Ctx) error {
+		name := c.Params("name")
+		return c.Status(200).SendString("Hello " + name)
+	})
 	app.Listen(":8000")
 }
